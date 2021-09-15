ced35fb6fc2297877162eff313469895c6a49935
KirC0ff <klarchencov@icloud.com>
2021-09-15 11:17:05 +0300

    Added opt4 in task1

---
+++ src/ru/mirea/task1/opt4/Main.java
@@ -0,0 +1,10 @@
+package ru.mirea.task1.opt4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Аргументы командной строки:");
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+    }
+}
